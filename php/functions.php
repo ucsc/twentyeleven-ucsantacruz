7ca1fd1481dfbe4159a5764e6ce4d1f60cd2d683
Rob Knight <raknight@ucsc.edu>
2014-10-29 17:00:04 -0700

    Remove custom background color theme option

--- php/functions.php
+++ php/functions.php
@@ -9,3 +9,12 @@ add_action( 'wp_enqueue_scripts', 'load_my_child_styles', 20 );
 function load_my_child_styles() {
     wp_enqueue_style( 'child-theme', get_stylesheet_uri() );
 }
+
+add_action( 'after_setup_theme', 'remove_features_from_child_theme', 20 ); 
+
+function remove_features_from_child_theme() {
+
+    // This will remove support for custom backgrounds
+    remove_theme_support( 'custom-background' );
+    
+}
